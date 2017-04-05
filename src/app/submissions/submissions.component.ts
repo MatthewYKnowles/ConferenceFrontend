5d2ff72b74dd51406d3a629d9fdff4221d2a1d1f
Matthew Knowles <matthewyknowles@gmail.com>
2017-04-05 11:11:07 -0400

    Added back in get all submissions

--- src/app/submissions/submissions.component.ts
+++ src/app/submissions/submissions.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {SubmissionService} from '../services/submission.service';
 
 @Component({
   selector: 'app-submissions',
   templateUrl: './submissions.component.html',
-  styleUrls: ['./submissions.component.css']
+  styleUrls: ['./submissions.component.css'],
+  providers: [SubmissionService]
 })
 export class SubmissionsComponent implements OnInit {
+  submissions: any;
 
-  constructor() { }
+  constructor(private _submissionService: SubmissionService) {
+    this._submissionService.getAllSubmissions().subscribe((submissions) => {
+      this.submissions = submissions;
+    });
+  }
 
   ngOnInit() {
   }
